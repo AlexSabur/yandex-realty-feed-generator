71b4973c586f890e553e2da381c05d7ffdb6a169
AlexSabur <alexsabur@live.ru>
2021-02-12 16:09:49 +0300

    сотка и гектар

--- src/Enums/Unit.php
+++ src/Enums/Unit.php
@@ -8,8 +8,12 @@ use MyCLabs\Enum\Enum;
  * Class Unit
  * @package Windstep\YRLGenerator\Enums
  * @method static METER_SQUARE
+ * @method static AR
+ * @method static HECTARE
  */
 class Unit extends Enum
 {
     private const METER_SQUARE = 'кв. м';
+    private const AR = 'сотка';
+    private const HECTARE = 'гектар';
 }
